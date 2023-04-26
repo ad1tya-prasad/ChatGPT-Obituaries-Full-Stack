5b00e506c249cca590170ffc062bb360c2c0c1ae
Aryan Karadia <aryan.karadia@ucalgary.ca>
2023-04-26 15:03:59 -0600

    music toggle

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 
 import Overlay from "./Overlay";
 
@@ -8,6 +7,8 @@ const Layout = () => {
   const navigate = useNavigate();
   const [inForm, setInForm] = useState(false);
   const [images, setImages] = useState([]);
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
 
   const createObituary = () => {
     setInForm(true);
@@ -28,6 +29,29 @@ const Layout = () => {
         );
         console.log(response.status); // check response status
         const data = await response.json();
+        setData(data);
+        setCount(data.length);
+        console.log(data); // check data being returned
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [inForm]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://atljom7p67ty535xlh7ygxv24m0ntzyj.lambda-url.ca-central-1.on.aws/",
+          {
+            method: "GET",
+          }
+        );
+        console.log(response.status); // check response status
+        const data = await response.json();
+        setData(data);
+        setCount(data.length);
         console.log(data); // check data being returned
       } catch (error) {
         console.error(error);
@@ -35,7 +59,6 @@ const Layout = () => {
     };
     fetchData();
   }, []);
-  
 
   return (
     <>
@@ -49,28 +72,42 @@ const Layout = () => {
         <h1 className="noObituary">No Obituary Yet.</h1>
       ) : (
         <div className="image-container">
-          {images.map((image, index) => (
+          {data.map((image, index) => (
             <div className="image" key={index}>
-              <img src={URL.createObjectURL(image.file)} alt={image.name} />
+              <img src={image.img} alt={image.name} />
               <h2>{image.name}</h2>
               <div className="dates">
-              <p className="birth-date">
-                {" "}
-                {new Date(image.dob).toLocaleDateString("en-US", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </p>
+                <p className="birth-date">
+                  {" "}
+                  {new Date(image.born).toLocaleDateString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
                 <p className="death-date">
-                {"‎ - "}
-                {new Date(image.dod).toLocaleDateString("en-US", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </p>
-            </div>
+                  {"‎ - "}
+                  {new Date(image.died).toLocaleDateString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
+                <p className="desc">{image.obituary}</p>
+                <button
+                  type="button"
+                  value="sound"
+                  className="play-button"
+                  id="music-toggle"
+                  onClick={() => {
+                    let audio = new Audio(image.voice);
+                    const btn = document.getElementById("music-toggle");
+                    audio.play();
+                  }}
+                >
+                  &#9658;
+                </button>
+              </div>
             </div>
           ))}
         </div>
